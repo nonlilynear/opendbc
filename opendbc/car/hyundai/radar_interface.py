0586135df388df9bae668d36216844530847a16d
royjr <royjr96@gmail.com>
2025-07-01 02:05:33 -0400

    Remove unused imports from radar_interface.py
    Cleaned up radar_interface.py by removing unused imports: math and HyundaiFlagsSP. This helps maintain code clarity and reduces unnecessary dependencies.

--- opendbc/car/hyundai/radar_interface.py
+++ opendbc/car/hyundai/radar_interface.py
@@ -6,7 +6,6 @@ from opendbc.car.interfaces import RadarInterfaceBase
 from opendbc.car.hyundai.values import DBC
 
 from opendbc.sunnypilot.car.hyundai.radar_interface_ext import RadarInterfaceExt
-from opendbc.sunnypilot.car.hyundai.values import HyundaiFlagsSP
 
 RADAR_START_ADDR = 0x500
 RADAR_MSG_COUNT = 32
