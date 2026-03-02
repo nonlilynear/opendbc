c89ef3c1a04b0eed335018326d20936fab32d9ea
royjr <royjr96@gmail.com>
2026-03-01 23:02:19 -0500

    HYUNDAI_KONA_EV_2ND_GEN  is MRR35_RADAR

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -334,7 +334,7 @@ class CAR(Platforms):
       HyundaiCarDocs("Hyundai Kona Electric (without HDA II) 2024", car_parts=CarParts.common([CarHarness.hyundai_a])),
     ],
     CarSpecs(mass=1740, wheelbase=2.66, steerRatio=13.6, tireStiffnessFactor=0.385),
-    flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE | HyundaiFlags.CCNC,
+    flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE | HyundaiFlags.CCNC | HyundaiFlags.MRR35_RADAR,
   )
   HYUNDAI_KONA_HEV = HyundaiPlatformConfig(
     [HyundaiCarDocs("Hyundai Kona Hybrid 2020", car_parts=CarParts.common([CarHarness.hyundai_i]))],  # TODO: check packages,
